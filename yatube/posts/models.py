5db78d4d1e4c548dd07f818900ab1835e68df152
Анжелика Булис <tvoya-kisa-33@yandex.ru>
2023-04-18 14:27:23 +0300

    Последние изменения в спринте

--- yatube/posts/models.py
+++ yatube/posts/models.py
@@ -1,3 +1,54 @@
+from django.contrib.auth import get_user_model
 from django.db import models
 
-# Create your models here.
+User = get_user_model()
+
+
+class Post(models.Model):
+    text = models.TextField(
+        verbose_name='текст записи'
+    )
+    pub_date = models.DateTimeField(
+        auto_now_add=True,
+        verbose_name='дата публикации'
+    )
+    author = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name='posts',
+        verbose_name='автор'
+    )
+    group = models.ForeignKey(
+        'Group',
+        on_delete=models.SET_NULL,
+        blank=True,
+        null=True,
+        related_name='posts',
+        verbose_name='сообщество'
+    )
+
+    class Meta:
+        ordering = ('-pub_date',)
+        verbose_name = 'пост'
+        verbose_name_plural = 'посты'
+
+
+class Group(models.Model):
+    title = models.CharField(
+        max_length=200,
+        verbose_name='заголовок'
+    )
+    slug = models.SlugField(
+        unique=True,
+        verbose_name='номер сообщества'
+    )
+    description = models.TextField(
+        verbose_name='описание сообщества'
+    )
+
+    class Meta:
+        verbose_name = 'сообщество'
+        verbose_name_plural = 'сообщества'
+
+    def __str__(self):
+        return self.title
--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -1,15 +1,27 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-# Create your views here.
+from django.shortcuts import get_object_or_404, render
+
+from .models import Group, Post
+
+AMOUNT_OF_POSTS: int = 10
 
 
 def index(request):
-    return HttpResponse('Главная страница')
-
-
-def group_list(request):
-    return HttpResponse('Названия групп')
+    posts = Post.objects.all()[:AMOUNT_OF_POSTS]
+    title = 'Последние обновления на сайте'
+    context = {
+        'posts': posts,
+        'title': title,
+    }
+    return render(request, 'posts/index.html', context)
 
 
 def group_posts(request, slug):
-    return HttpResponse(f'Записи сообщества {slug}')
+    group = get_object_or_404(Group, slug=slug)
+    posts = group.posts.select_related('author')[:AMOUNT_OF_POSTS]
+    title = f'Записи сообщества {slug}'
+    context = {
+        'group': group,
+        'posts': posts,
+        'title': title,
+    }
+    return render(request, 'posts/group_list.html', context)
